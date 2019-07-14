2af791c5e9ebb988860e9848a4a3b93df19da85c
kallard <contact@allard-kevin.fr>
2019-07-14 16:10:34 +0200

    feat: ajout de la modification de la description de l'utilisateur

---
+++ src/Form/Type/ChangeDescriptionType.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ChangeDescriptionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('description', TextareaType::class)
+        ;
+    }
+}
