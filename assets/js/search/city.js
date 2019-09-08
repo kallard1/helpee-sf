f1bb25d7fbb7679cab7afd799e9945ce252e3e03
kallard <contact@allard-kevin.fr>
2019-09-08 13:52:00 +0200

    fix: select

--- assets/js/search/city.js
+++ assets/js/search/city.js
@@ -1,40 +1,41 @@
-const $ = require('jquery')
-const select2 = require('select2')
+const $ = require('jquery');
+const select2 = require('select2');
 
-$(document).ready(function () {
+$(document).ready(function() {
   $('.js-data-cities').select2({
     theme: 'bootstrap4',
     ajax: {
       url: '/search/city',
       dataType: 'json',
       delay: 250,
-      data: function (params) {
+      data: function(params) {
         return {
           q: params.term,
-          page: params.page
-        }
+          page: params.page,
+        };
       },
-      processResults: function (data, params) {
-        params.page = params.page || 1
+      processResults: function(data, params) {
+        params.page = params.page || 1;
         return {
           results: data,
           pagination: {
-            mode: (params.page * 30) < data.length
-          }
-        }
+            mode: (params.page * 30) < data.length,
+          },
+        };
       },
-      cache: true
+      cache: true,
     },
-    escapeMarkup: function (markup) { return markup },
+    escapeMarkup: function(markup) { return markup; },
+    minimumResultsForSearch: -1,
     minimumInputLength: 2,
     templateResult: formatRepo,
-    templateSelection: formatRepoSelection
-  })
-})
+    templateSelection: formatRepoSelection,
+  });
+});
 
-function formatRepo (repo) {
+function formatRepo(repo) {
   if (repo.loading) {
-    return repo.text
+    return repo.text;
   }
 
   const $container = $(`
@@ -42,13 +43,14 @@ function formatRepo (repo) {
     <div class='select2-result-repository__meta'>
         <div class='select2-result-repository__title'></div>
       </div>
-    </div>`)
+    </div>`);
 
-  $container.find(".select2-result-repository__title").text(`${repo.zip_code} - ${repo.name}`)
+  $container.find('.select2-result-repository__title')
+      .text(`${repo.zip_code} - ${repo.name}`);
 
   return $container;
 }
 
-function formatRepoSelection (repo) {
-  return repo.name || repo.text
+function formatRepoSelection(repo) {
+  return repo.name || repo.text;
 }
